4aa0660d0b889bc25c0cc10e191bee37dc87c4b5
gem-RishabhZutshi <rishabh.zutshi8@gmail.com>
2024-11-19 22:08:22 +0530

    Infra fix4 debug

--- Backend/modules/ECS_Taskdefinations/outputs.tf
+++ Backend/modules/ECS_Taskdefinations/outputs.tf
@@ -1,3 +1,7 @@
 output "ecommerce_backend_taskdef_arn" {
     value = aws_ecs_task_definition.ecommerce_backend.arn
 }
+
+output "ecs_task_image" {
+  value = "${replace(jsonencode("${var.ecommerce_backend_ecr_repository_url}:${var.image_version}"), "/\"([0-9]+\\.?[0-9]*)\"/", "$1")}"
+}
